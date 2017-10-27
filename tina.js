45e74b4fe548887dabbd0d88ce93387ccada3dd6
Tina Huang <tingyuhuang@aol.com>
2017-10-27 09:54:32 -0700

    saved file

--- tina.js
+++ tina.js
@@ -2,3 +2,11 @@ function encrypt(string) {
   newString = string.slice(3) + string.slice(0, 3).toUpperCase();
   return newString;
 }
+
+function decrypt(string) {
+  let stringLength = string.length;
+  newString =
+    string.slice(stringLength - 3).toLowerCase() +
+    string.slice(0, stringLength - 3);
+  return newString;
+}
